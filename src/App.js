4b320dfe53b3485c3575bc523d6fccf37bec3cef
Hansel <valkirecesa@gmail.com>
2023-02-04 20:38:24 +0800

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import SampleTable from './SampleTable';
+import { Tabs } from 'antd';
 
 function App() {
+
+  const handleTabChange = (key) => {
+    console.log(key)
+  }
+
+  const defaultColumns = [
+    {
+        title: 'Certificate Number',
+        dataIndex: 'certificateNumber',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.certificateNumber.localeCompare(b.certificateNumber),
+    },
+    {
+        title: 'Customer Name',
+        dataIndex: 'customerName',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.customerName.localeCompare(b.customerName),
+    },
+    {
+        title: 'MSSL',
+        dataIndex: 'mssl',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.mssl.localeCompare(b.mssl),
+    },
+    {
+        title: 'Geneco Account Number',
+        dataIndex: 'genecoAccountNumber',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.genecoAccountNumber.localeCompare(b.genecoAccountNumber),
+    },
+    {
+        title: 'Contact Number',
+        dataIndex: 'contactNumber',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.contactNumber.localeCompare(b.contactNumber),
+    },
+    {
+        title: 'Product Number',
+        dataIndex: 'productNumber',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.productNumber.localeCompare(b.productNumber),
+    },
+    {
+        title: 'Certificate Issuance Date',
+        dataIndex: 'certificateIssuanceDate',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => new Date(a.certificateIssuanceDate) - new Date(b.certificateIssuanceDate),
+    },
+    {
+        title: 'Start Date',
+        dataIndex: 'startDate',
+        editable: true,
+        sorter: (a, b) => new Date(a.startDate) - new Date(b.startDate),
+    },
+    {   
+        title: 'End Date',
+        dataIndex: 'endDate',
+        editable: true,
+        sorter: (a, b) => new Date(a.endDate) - new Date(b.endDate),
+    },
+    {
+        title: 'Total Quantity',
+        dataIndex: 'totalQuantity',
+        editable: true,
+        sorter: (a, b) => a.totalQuantity - b.totalQuantity,
+    },
+    {
+        title: 'Certificate',
+        dataIndex: 'certificate',
+        editable: true,
+    },
+    {
+        title: 'Notification Email Sent To',
+        dataIndex: 'notifiedEmail',
+        editable: true,
+        sortDirections: ['ascend', 'descend', 'ascend'],
+        sorter: (a, b) => a.notifiedEmail.localeCompare(b.notifiedEmail),
+    }
+  ];
+
+
+
+  const tabs = [
+    {
+      key: 1,
+      label: 'Residential CC Cert Issued',
+      children: <SampleTable defaultColumns={defaultColumns}></SampleTable>,
+    },
+    {
+      key: 2,
+      label: 'Residential REC Cert Issued',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 10)]}></SampleTable>,
+    },
+    {
+      key: 3,
+      label: 'Residential Cert Failed to Issue',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 6)]}></SampleTable>,
+    },
+    {
+      key: 4,
+      label: '(C&I) CC Cert Issued',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 4)]}></SampleTable>,
+    },
+    {
+      key: 5,
+      label: '(C&I) REC Cert Issued',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 9)]}></SampleTable>,
+    },
+    {
+      key: 6,
+      label: '(C&I) SG REC Cert Issued',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 11)]}></SampleTable>,
+    },
+    {
+      key: 7,
+      label: '(C&I) Cert Failed to Issue',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 5)]}></SampleTable>,
+    },
+    {
+      key: 8,
+      label: 'Ad-hoc Cert Issued',
+      children: <SampleTable defaultColumns={[...defaultColumns.slice(0, 7)]}></SampleTable>,
+    }
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs defaultActiveKey='1' items={tabs} onChange={handleTabChange}></Tabs>
     </div>
   );
 }
---
+++ src/SampleTable.js
@@ -0,0 +1,233 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState, useEffect, useContext, createContext, useRef } from "react";
+import { Form, Input, Table, Button, Space } from 'antd';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
+import { dummyData } from "./assets/dummy";
+
+const EditableContext = createContext(null);
+
+const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+        <Form form={form} component={false}>
+            <EditableContext.Provider value={form}>
+                <tr {...props} />
+            </EditableContext.Provider>
+        </Form>
+    );
+};
+
+const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+}) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+    useEffect(() => {
+        if (editing) {
+            inputRef.current.focus();
+        }
+    }, [editing]);
+    const toggleEdit = () => {
+        setEditing(!editing);
+        form.setFieldsValue({
+            [dataIndex]: record[dataIndex],
+        });
+    };
+    const save = async () => {
+        try {
+            const values = await form.validateFields();
+            toggleEdit();
+            handleSave({
+                ...record,
+                ...values,
+            });
+        } catch (errInfo) {
+            console.log('Save failed:', errInfo);
+        }
+    };
+    let childNode = children;
+    if (editable) {
+        childNode = editing ? (
+            <Form.Item
+                style={{
+                    margin: 0,
+                }}
+                name={dataIndex}
+                rules={[
+                    {
+                        required: true,
+                        message: `${title} is required.`,
+                    },
+                ]}
+            >
+                <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+            </Form.Item>
+        ) : (
+            <div
+                className="editable-cell-value-wrap"
+                style={{
+                    paddingRight: 24,
+                }}
+                onClick={toggleEdit}
+            >
+                {children}
+            </div>
+        );
+    }
+    return <td {...restProps}>{childNode}</td>;
+};
+
+
+const SampleTable = ({ defaultColumns }) => {
+
+    const [dataSource, setDataSource] = useState([...dummyData]);
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText((selectedKeys.length === 0 && '') || selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters, confirm, dataIndex) => {
+        clearFilters();
+        handleSearch([], confirm, dataIndex)
+    };
+
+    const handleSave = (row) => {
+        console.log(row);
+        const newData = [...dataSource];
+        const index = newData.findIndex((item) => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, {
+            ...item,
+            ...row,
+        });
+        setDataSource(newData);
+    };
+
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters, confirm, dataIndex)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1890ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
+
+    const components = {
+        body: {
+            row: EditableRow,
+            cell: EditableCell,
+        },
+    };
+
+    const columns = defaultColumns.map((col) => {
+        if (!col.editable) {
+            return col;
+        }
+        // If the column is editable, i.e., editable attribute is true,
+        // add an onCell attribute that sets new props to the cells
+        // of that column
+        return {
+            ...col,
+            ...getColumnSearchProps(col.dataIndex),
+            onCell: (record) => ({
+                record,
+                editable: col.editable,
+                dataIndex: col.dataIndex,
+                title: col.title,
+                handleSave,
+            }),
+        };
+    });
+
+
+    return (
+        <div style={{ 'overflowX': 'auto' }}>
+            <Table
+                components={components}
+                rowClassName={() => 'editable-row'}
+                bordered
+                dataSource={dataSource}
+                columns={columns}
+                pagination={{pageSize: 5}}
+            />
+        </div>
+    );
+}
+
+export default SampleTable;
